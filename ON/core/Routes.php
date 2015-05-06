7c130b7e79b10b0ade495ee3b2a1414fa79f4c2c
Patrick Kaminski <patrix@users.noreply.github.com>
2015-05-06 20:01:27 -0300

    Adicionando arquivos do Composer
    Adicionando classe de roteamento e arquivos do Composer

---
+++ ON/core/Routes.php
@@ -0,0 +1,15 @@
+<?php
+    namespace ON;
+
+	class Routes
+    {
+	  private function __construct()
+      {
+	  }
+	  
+	  public static function add($origem, $destino)
+      {
+		$request=Request::request();
+		$_SERVER['REQUEST_URI']=str_replace($origem, $destino, $request);
+	  }
+	}
