61f8e6bd75bcc6b6614ea4f273e01f4b8d1c6757
Patrick Kaminski <patrick@patrickkaminski.com>
2017-10-19 21:03:47 -0200

    Ajustes Composer

--- ON/autoload.php
+++ ON/autoload.php
@@ -6,57 +6,18 @@
      */
     function onLoader($class)
     {
+        if (!defined('PATH')):
+            define('PATH', dirname(__FILE__).DS);
+        endif;
         if (strpos($class, 'ON')===TRUE):
             return;
         endif;
         $class=str_replace('ON\\', '', $class);
-        if ($class != 'ON'):
-            $classfile = PATH.'core\\'.$class.'.php';
-        else:
-            $classfile = PATH.$class.'.php';
-        endif;
+        $classfile = PATH.'core\\'.$class.'.php';
         if (file_exists($classfile)):
             include_once($classfile);
         else:
             //throw new Exception('[Error autoload] File not found ('.$classfile.')');
-            //echo '[Error autoload] File not found ('.$classfile.')';
         endif;
     }
     spl_autoload_register('onLoader');
-
-
-
-
-//exit;
-
-	/*function __autoload($nomeClasse){
-		echo $nomeClasse;
-	}*/
-/*	function __autoload($lib=NULL) {
-		if (is_null($lib)):
-			throw new Exception('[Erro RO19] Biblioteca nao informada');
-		else:
-			//$libfile='core/general/'.$lib.'.php';
-			$libfile=$lib.'.php';
-			echo PATH.$libfile;
-			if (file_exists(PATH.$libfile)):
-				include_once(PATH.$libfile);
-			else:
-				throw new Exception('[Erro RO25] Biblioteca nao encontrada ('.$libfile.') ');
-			endif;
-		endif;
-	}*/
-/*
-public static function LoadContainer ($lib=NULL) {
-	if(is_null($lib)):
-	throw new Exception('[Erro RO6] Tipo de Container nao informado');
-	else:
-	$libfile='core/apps/'.$lib.'.php';
-	if(file_exists(PATH.$libfile)):
-	include_once($libfile);
-	else:
-	throw new Exception('[Erro RO12] Tipo de Container nao existente ('.$libfile.') ');
-	endif;
-	endif;
-}
-*/
