b175acb46963493913850ff1fbdcd7f4f3906afb
Patrick Kaminski <patrick@patrickkaminski.com>
2018-01-23 22:23:09 -0200

    Ajustes nos testes unitários

---
+++ tests/assets/models/sqlite.php
@@ -0,0 +1,9 @@
+<?php
+$this->_dsntype=2;
+$this->_driver = 'sqlite';
+$this->_user = '';
+$this->_pass = '';
+$this->_host = '';
+$this->_database = 'sqlite:tests/assets/models/teste.sqlite';
+$this->_dsn = 'sqlite:tests/assets/models/teste.sqlite';
+$this->_driveroptions = null;
